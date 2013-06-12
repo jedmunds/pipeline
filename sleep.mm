245cba85818d76769ff900060496fee75959ad45
jedmunds <jedmunds24t@gmail.com>
2013-06-12 08:00:51 -0700

    Personal documents that need upload

---
+++ sleep.mm
@@ -0,0 +1,553 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1370148804297" ID="ID_1324003294" MODIFIED="1370266079055" TEXT="Everyman">
+<node CREATED="1370148827811" ID="ID_1383958140" MODIFIED="1370226792321" POSITION="right" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Everyman Meal Plan
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1370150020280" ID="ID_402092020" MODIFIED="1370212475902">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Week1/2
+    </p>
+    <p>
+      Shopping List ---
+    </p>
+    <p>
+      Hot Dogs
+    </p>
+    <p>
+      Baked beans
+    </p>
+    <p>
+      Eggs
+    </p>
+    <p>
+      Tomato
+    </p>
+    <p>
+      Cooked ham
+    </p>
+    <p>
+      Frozen veggies
+    </p>
+    <p>
+      grated cheese
+    </p>
+    <p>
+      spinach
+    </p>
+    <p>
+      mushrooms
+    </p>
+    <p>
+      corn tortillas
+    </p>
+    <p>
+      black beans
+    </p>
+    <p>
+      ground lamb
+    </p>
+    <p>
+      broccoli
+    </p>
+    <p>
+      ranch dressing
+    </p>
+    <p>
+      esparagus
+    </p>
+    <p>
+      salmon
+    </p>
+    <p>
+      grapes
+    </p>
+    <p>
+      honeydew
+    </p>
+    <p>
+      blueberries
+    </p>
+    <p>
+      almonds
+    </p>
+    <p>
+      hummus
+    </p>
+    <p>
+      celery
+    </p>
+    <p>
+      lettuce
+    </p>
+    <p>
+      white potatoes
+    </p>
+    <p>
+      brown rice
+    </p>
+    <p>
+      onion
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1370148867530" ID="ID_464074938" MODIFIED="1370212417775" TEXT="Day1/8">
+<node CREATED="1370148955706" ID="ID_1339723860" MODIFIED="1370214143073" TEXT="Breakfast">
+<node CREATED="1370149254548" ID="ID_1707603084" MODIFIED="1370149358502">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Omelette: 3 eggs, tomato, ham, veggies, cheese
+    </p>
+    <p>
+      spinach w/ cheese + sauteed mushrooms
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1370148965062" ID="ID_1836170937" MODIFIED="1370214143075" TEXT="Lunch">
+<node CREATED="1370149311482" ID="ID_1744776626" MODIFIED="1370212795047">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Meat Burrito: Corn tortilla, black beans, ground lamb, cheese, salsa
+    </p>
+    <p>
+      Broccoli + ranch
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370148967030" ID="ID_65777189" MODIFIED="1370214143077" TEXT="Dinner">
+<node CREATED="1370149513067" ID="ID_847927634" MODIFIED="1370149579218">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tacos:&#160;Black beans, cheese, ground lamb, tomatoes
+    </p>
+    <p>
+      Sauteed esparagus + leftover mushrooms
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1370148968966" ID="ID_1787418774" MODIFIED="1370214143079" TEXT="Supper">
+<node CREATED="1370149587587" ID="ID_943872373" MODIFIED="1370209062400">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      salmon
+    </p>
+    <p>
+      Fruit Bowl: grapes, honeydew, blueberries
+    </p>
+    <p>
+      Spinach
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370150337677" ID="ID_1299089850" MODIFIED="1370214143080" TEXT="Snacks">
+<node CREATED="1370150345566" ID="ID_960273111" MODIFIED="1370150866327" TEXT="Almonds, fruit, celery w/ hummus + peanut butter"/>
+</node>
+</node>
+<node CREATED="1370148905762" ID="ID_833980710" MODIFIED="1370212424572" TEXT="Day2/9">
+<node CREATED="1370148975014" ID="ID_146779004" MODIFIED="1370214143082" TEXT="Breakfast">
+<node CREATED="1370209023304" ID="ID_1007836164" MODIFIED="1370214093210">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Salmon
+    </p>
+    <p>
+      Black beans+cheese+rice
+    </p>
+    <p>
+      Mushroom mix:
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370148978206" ID="ID_1540384218" MODIFIED="1370214143086" TEXT="Lunch">
+<node CREATED="1370209185318" ID="ID_268760586" MODIFIED="1370209264993">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Burrito: egg, black beans, cheese, tomato
+    </p>
+    <p>
+      Lettuce and tomato salad
+    </p>
+    <p>
+      Fruit Bowl: grapes, honeydew, blueberries
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370148979542" ID="ID_1857063249" MODIFIED="1370214143088" TEXT="Dinner">
+<node CREATED="1370209440760" ID="ID_1860934947" MODIFIED="1370209474072">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hot Dogs and Beans
+    </p>
+    <p>
+      Spinach + fruit bowl
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370148980854" ID="ID_354670933" MODIFIED="1370214143090" TEXT="Supper">
+<node CREATED="1370209479165" ID="ID_397626344" MODIFIED="1370209627695">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Spinach soup: http://lowfatcooking.about.com/od/lowfatsoups/r/spinachsoup.htm
+    </p>
+    <p>
+      Fruit Bowl + healthy snack
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370209632905" ID="ID_1657938271" MODIFIED="1370214143090" TEXT="Snacks">
+<node CREATED="1370209636072" ID="ID_693504188" MODIFIED="1370209638256" TEXT="Same as above"/>
+</node>
+</node>
+<node CREATED="1370148910243" ID="ID_1316591554" MODIFIED="1370212432629" TEXT="Day3/10">
+<node CREATED="1370209889629" ID="ID_1888005307" MODIFIED="1370214143092" TEXT="Breakfast">
+<node CREATED="1370209947899" ID="ID_377326404" MODIFIED="1370210014054">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Fried Rice: Brown rice,&#160;ham, onion, frozen veggies, mushrooms, esparagus
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370209897391" ID="ID_1843633207" MODIFIED="1370214143095" TEXT="Lunch">
+<node CREATED="1370211317277" ID="ID_173898846" MODIFIED="1370212230608">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Burrito: egg, black beans, cheese, tomato
+    </p>
+    <p>
+      Mixed Salad: Lettuce, tomato, broccoli, ham, sliced carrots/beets
+    </p>
+    <p>
+      Fruit Bowl: grapes, honeydew, blueberries
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370209898815" ID="ID_915732396" MODIFIED="1370214143095" TEXT="Dinner">
+<node CREATED="1370210124611" ID="ID_1894072671" MODIFIED="1370210136928" TEXT="Fried Rice: leftovers (make extra)"/>
+</node>
+<node CREATED="1370209900191" ID="ID_1985335061" MODIFIED="1370214143097" TEXT="Supper">
+<node CREATED="1370211611920" ID="ID_1253080248" MODIFIED="1370211690799">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hash brown mix: Hash browns, eggs, frozen veggies, mushrooms
+    </p>
+    <p>
+      Spinach + fruit bowl
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370209902063" ID="ID_1164310544" MODIFIED="1370214143098" TEXT="Snacks">
+<node CREATED="1370211536063" ID="ID_1068581690" MODIFIED="1370211539041" TEXT="Same as above"/>
+</node>
+</node>
+<node CREATED="1370148913663" ID="ID_1962621168" MODIFIED="1370212438264" TEXT="Day4/11">
+<node CREATED="1370211923166" ID="ID_904173389" MODIFIED="1370214143100" TEXT="Breakfast">
+<node CREATED="1370211932716" ID="ID_204272326" MODIFIED="1370212027803">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Chicken mix: Chicken breast, bell peppers, esparagus, mushrooms, spices
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370211926129" ID="ID_1032196852" MODIFIED="1370214143102" TEXT="Lunch">
+<node CREATED="1370212035140" ID="ID_1027767537" MODIFIED="1370212083453">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Breakfast burrito: egg, black beans, cheese, tomato
+    </p>
+    <p>
+      Leftover chicken mix
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370211927385" ID="ID_609503263" MODIFIED="1370214143104" TEXT="Dinner">
+<node CREATED="1370212097907" ID="ID_1171817727" MODIFIED="1370212150245">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hot dogs and beans
+    </p>
+    <p>
+      Mixed Salad: Lettuce, tomato, broccoli, cooked egg
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370211928673" ID="ID_1745506682" MODIFIED="1370214143106" TEXT="Supper">
+<node CREATED="1370212273215" ID="ID_1089603048" MODIFIED="1370212297017">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hash Brown Mix: Hash browns, eggs, frozen veggies, mushrooms
+    </p>
+    <p>
+      Fruit Bowl + spinach
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370212300711" ID="ID_1452430367" MODIFIED="1370214143107" TEXT="Snacks">
+<node CREATED="1370212303874" ID="ID_351012232" MODIFIED="1370212306517" TEXT="Same as above"/>
+</node>
+</node>
+<node CREATED="1370148915088" ID="ID_1823319483" MODIFIED="1370212443601" TEXT="Day5/12">
+<node CREATED="1370213892083" ID="ID_481840268" MODIFIED="1370214143109" TEXT="Breakfast">
+<node CREATED="1370213969098" ID="ID_292701256" MODIFIED="1370214058969">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Breakfast burrito: egg, black bean, cheese, tomato
+    </p>
+    <p>
+      Mushroom mix: mushrooms, esparagus, oninions, bell peppers
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370213897673" ID="ID_345464080" MODIFIED="1370213898639" TEXT="Lunch">
+<node CREATED="1370214670616" ID="ID_1865036668" MODIFIED="1370214737817">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Omelette: 3 eggs, ham, frozen veggies, cheese, tomato
+    </p>
+    <p>
+      Spinach and cheese
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370213898913" ID="ID_715468319" MODIFIED="1370213899982" TEXT="Dinner">
+<node CREATED="1370225679338" ID="ID_1304406493" MODIFIED="1370225695974" TEXT="Fried Rice: Brown rice, ham, onion, frozen veggies, mushrooms, esparagus"/>
+</node>
+<node CREATED="1370213900233" ID="ID_697494805" MODIFIED="1370213901718" TEXT="Supper">
+<node CREATED="1370225747193" ID="ID_234377048" MODIFIED="1370225770360">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Spinach soup: http://lowfatcooking.about.com/od/lowfatsoups/r/spinachsoup.htm
+    </p>
+    <p>
+      Fruit bowl
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1370213902000" ID="ID_823711872" MODIFIED="1370213903510" TEXT="Snacks">
+<node CREATED="1370225705150" ID="ID_247238601" MODIFIED="1370225709388" TEXT="leftover friend rice"/>
+</node>
+</node>
+<node CREATED="1370148916800" ID="ID_290669604" MODIFIED="1370212448809" TEXT="Day6/13"/>
+<node CREATED="1370148919007" ID="ID_875646008" MODIFIED="1370212453736" TEXT="Day7/14"/>
+</node>
+</node>
+<node CREATED="1370148834841" ID="ID_1104382762" MODIFIED="1370266068460" POSITION="left" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Everyman&#160;Schedule
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1370266082573" ID="ID_1182557850" LINK="Documents/week1.html" MODIFIED="1370269466628" TEXT="Week1">
+<node CREATED="1370266092920" ID="ID_911334922" MODIFIED="1370266096162" TEXT="Day1"/>
+<node CREATED="1370266103410" ID="ID_529906719" MODIFIED="1370266105186" TEXT="Day2"/>
+<node CREATED="1370266107580" ID="ID_1755831220" MODIFIED="1370266108882" TEXT="Day3"/>
+<node CREATED="1370266109508" ID="ID_645298417" MODIFIED="1370266112194" TEXT="Day4"/>
+<node CREATED="1370266112460" ID="ID_136516828" MODIFIED="1370266114025" TEXT="Day5"/>
+<node CREATED="1370266114348" ID="ID_1794097307" MODIFIED="1370266116050" TEXT="Day6"/>
+<node CREATED="1370266116308" ID="ID_462850022" MODIFIED="1370266117986" TEXT="Day7"/>
+</node>
+<node CREATED="1370266122367" ID="ID_1508617955" MODIFIED="1370266128204" TEXT="Week1">
+<node CREATED="1370210407883" ID="ID_1280574536" MODIFIED="1370210415233" TEXT="Odd days">
+<node CREATED="1370210304879" ID="ID_1606971087" MODIFIED="1370269525066">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Supper - 12:30am
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1370269633394" ID="ID_493488422" MODIFIED="1370269647296" TEXT="45min walk/15 min run - 1:15am"/>
+<node CREATED="1370269535124" ID="ID_254511399" MODIFIED="1370269667551" TEXT="Project - 2:00am"/>
+<node CREATED="1370269481605" ID="ID_945364517" MODIFIED="1370269517402" TEXT="Nap - 4:15am"/>
+<node CREATED="1370210338126" ID="ID_1735975779" MODIFIED="1370227011742" TEXT="Breakfast - 4:35am"/>
+<node CREATED="1370210440171" ID="ID_237929757" MODIFIED="1370227046453" TEXT="45 min walk/15 min run - 5:20am"/>
+<node CREATED="1370210464120" ID="ID_1629672193" MODIFIED="1370227060589" TEXT="Shower - 6:05am"/>
+<node CREATED="1370210502226" ID="ID_1965604818" MODIFIED="1370227066160" TEXT="Project - 6:35am"/>
+<node CREATED="1370210522021" ID="ID_138451448" MODIFIED="1370227078205" TEXT="Nap - 8:20am"/>
+<node CREATED="1370210532445" ID="ID_978594175" MODIFIED="1370227088365" TEXT="Lunch - 8:40am"/>
+<node CREATED="1370210543476" ID="ID_1861815815" MODIFIED="1370227101276" TEXT="45 min walk/15 min run - 9:25am"/>
+<node CREATED="1370210576040" ID="ID_1190805628" MODIFIED="1370227125239" TEXT="Project - 10:10am"/>
+<node CREATED="1370210620510" ID="ID_324551469" MODIFIED="1370227133782" TEXT="Nap - 2:40pm"/>
+<node CREATED="1370210634173" ID="ID_1108785146" MODIFIED="1370227139766" TEXT="Dinner - 3:00pm"/>
+<node CREATED="1370210644037" ID="ID_1266595076" MODIFIED="1370227145486" TEXT="45 min walk/15 min run - 3:45pm"/>
+<node CREATED="1370210656125" ID="ID_343920688" MODIFIED="1370227151633" TEXT="Shower - 4:30pm"/>
+<node CREATED="1370210665227" ID="ID_154462647" MODIFIED="1370227158182" TEXT="Project - 5:00pm"/>
+<node CREATED="1370210676734" ID="ID_975347449" MODIFIED="1370227166301" TEXT="Nap - 9:00pm"/>
+<node CREATED="1370269598787" ID="ID_1579517882" MODIFIED="1370269598787" TEXT=""/>
+</node>
+<node CREATED="1370210429547" ID="ID_179891451" MODIFIED="1370210432265" TEXT="Even days">
+<node CREATED="1370210753903" ID="ID_417213083" MODIFIED="1370269563001" TEXT="Supper - 12:30am"/>
+<node CREATED="1370269606906" ID="ID_1926021204" MODIFIED="1370269622268" TEXT="30min Walk - 1:15am"/>
+<node CREATED="1370269622914" ID="ID_506606152" MODIFIED="1370269628575" TEXT="Project - 1:45am"/>
+<node CREATED="1370269550452" ID="ID_623211751" MODIFIED="1370269555777" TEXT="Nap - 4:15am"/>
+<node CREATED="1370210760064" ID="ID_1336790426" MODIFIED="1370227227755" TEXT="Breakfast - 4:35am"/>
+<node CREATED="1370210766544" ID="ID_251719589" MODIFIED="1370227351010" TEXT="Insanity Workout - 6:20am"/>
+<node CREATED="1370211431170" ID="ID_282162411" MODIFIED="1370227356594" TEXT="Small protein shake - 7:05am"/>
+<node CREATED="1370211008650" ID="ID_1008286514" MODIFIED="1370227360209" TEXT="Shower - 7:20am"/>
+<node CREATED="1370211020148" ID="ID_1586769336" MODIFIED="1370227374313" TEXT="Project - 7:50am"/>
+<node CREATED="1370211035101" ID="ID_501648048" MODIFIED="1370269280674" TEXT="Nap - 8:20am"/>
+<node CREATED="1370211042372" ID="ID_169283819" MODIFIED="1370269276872" TEXT="Lunch - 8:40am"/>
+<node CREATED="1370211051039" ID="ID_1871066101" MODIFIED="1370269284336" TEXT="30 min walk - 9:25am"/>
+<node CREATED="1370211074784" ID="ID_503957708" MODIFIED="1370269272984" TEXT="Project - 9:55 am"/>
+<node CREATED="1370211089012" ID="ID_574211505" MODIFIED="1370227328178" TEXT="Nap - 2:40pm"/>
+<node CREATED="1370211095708" ID="ID_98032731" MODIFIED="1370227333849" TEXT="Dinner - 3:00pm"/>
+<node CREATED="1370211100396" ID="ID_1083272891" MODIFIED="1370227405559" TEXT="Insanity Workout - 4:45pm"/>
+<node CREATED="1370211463770" ID="ID_674609620" MODIFIED="1370227411491" TEXT="Small protein shake - 5:30pm"/>
+<node CREATED="1370211135490" ID="ID_1377502413" MODIFIED="1370227418518" TEXT="Shower - 5:45pm"/>
+<node CREATED="1370211141450" ID="ID_889137729" MODIFIED="1370227423902" TEXT="Project - 6:15pm"/>
+<node CREATED="1370211152033" ID="ID_638672967" MODIFIED="1370227433736" TEXT="Nap - 9:00pm"/>
+</node>
+</node>
+</node>
+<node CREATED="1370211219823" ID="ID_67973718" LINK="project.mm" MODIFIED="1370211288178" POSITION="right" STYLE="bubble" TEXT="Project"/>
+</node>
+</map>
